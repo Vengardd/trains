198c4864344c018fe2a0987c484a7809e49610fc
Vengardd-Laptop <cesarz.krawczyk@gmail.com>
2018-11-07 12:20:46 +0100

    added SameConnectrionException, handle this Exception in TrainService

---
+++ src/main/java/com/vengard/trains/exception/SameConnectionException.java
@@ -0,0 +1,4 @@
+package com.vengard.trains.exception;
+
+public class SameConnectionException extends RuntimeException {
+}
--- src/main/java/com/vengard/trains/service/TrainService.java
+++ src/main/java/com/vengard/trains/service/TrainService.java
@@ -1,5 +1,6 @@
 package com.vengard.trains.service;
 
+import com.vengard.trains.exception.SameConnectionException;
 import com.vengard.trains.exception.TrainExistingException;
 import com.vengard.trains.model.Train;
 import com.vengard.trains.repository.TrainRepository;
@@ -15,6 +16,8 @@ public class TrainService {
     public Train addTrain(Train train) {
         if(trainRepository.findTrain(train).isPresent())
             throw new TrainExistingException();
+        if(train.getTrain().get(0).equals(train.getTrain().get(1))) //ToDo better handle this exception
+            throw new SameConnectionException();
         return trainRepository.addTrain(train);
     }
 
