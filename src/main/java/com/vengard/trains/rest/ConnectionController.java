5a7d6c9d49bed532b4f496043c412925d2a24344
Vengardd-Laptop <cesarz.krawczyk@gmail.com>
2018-11-07 20:35:24 +0100

    added @RequestParam in getConnection method in ConnectionController

--- src/main/java/com/vengard/trains/rest/ConnectionController.java
+++ src/main/java/com/vengard/trains/rest/ConnectionController.java
@@ -2,6 +2,7 @@ package com.vengard.trains.rest;
 
 import com.vengard.trains.model.Connection;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,8 @@ public class ConnectionController {
 
 
     @GetMapping("/api/shortest_rules")
-    public Connection getConnection() {
+    public Connection getConnection(@RequestParam("start") String start, @RequestParam("destination") String destination) {
         return new Connection();
     }
+
 }
