21c14c8349c2b577e901e4a522dd66755d0a3341
Vengardd-Laptop <cesarz.krawczyk@gmail.com>
2018-11-07 14:56:45 +0100

    added Connection clsas, method getConnection in TrainController

---
+++ src/main/java/com/vengard/trains/model/Connection.java
@@ -0,0 +1,34 @@
+package com.vengard.trains.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+
+    private List<Train> routes = new ArrayList<>();
+    private int distance;
+
+    public Connection() {
+    }
+
+    public Connection(List<Train> routes, int distance) {
+        this.routes = routes;
+        this.distance = distance;
+    }
+
+    public List<Train> getRoutes() {
+        return routes;
+    }
+
+    public void setRoutes(List<Train> routes) {
+        this.routes = routes;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+}
--- src/main/java/com/vengard/trains/rest/TrainController.java
+++ src/main/java/com/vengard/trains/rest/TrainController.java
@@ -1,5 +1,6 @@
 package com.vengard.trains.rest;
 
+import com.vengard.trains.model.Connection;
 import com.vengard.trains.model.Train;
 import com.vengard.trains.service.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ConcurrentModificationException;
 
 @RestController
 public class TrainController {
@@ -22,6 +25,11 @@ public class TrainController {
     @PostMapping("/api/trains")
     public Train addTrain(@RequestBody Train train) {
         return trainService.addTrain(train);
+    }
+
+    @GetMapping("/api/shortest_rules")
+    public Connection getConnection() {
+        return new Connection();
     }
 
 
